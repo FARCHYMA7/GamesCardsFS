e3d0ccfbee63754e407579c83e002221c3f55ac6
FARCHYMU7 <matan.farchy@gmail.com>
2024-05-24 19:05:55 +0300

    Last Version

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-document.getElementById('go-button').addEventListener('click', async () => {
-    const gamesContainer = document.getElementById('games-container');
+document.getElementById('goButton').addEventListener('click', async () => {
+    const gamesContainer = document.getElementById('gamesContainer');
     gamesContainer.innerHTML = ''; // Clear previous games
   
     try {
       const response = await fetch('http://localhost:3030/games');
       const games = await response.json();
+      gamesLen = games.length
   
-      games.slice(0, 9).forEach((game) => {
+      games.forEach((game) => {
   
         const gameCard = document.createElement('div');
-        gameCard.className = 'game-card';
+        gameCard.className = 'gameCard';
         gameCard.innerHTML = `
           <h3>${game.title}</h3>
           <img src="${game.thumbnail}" alt="${game.title}">
